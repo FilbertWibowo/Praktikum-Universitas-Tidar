f05fa96245b19c7d5188d59cff4da745a44be880
Filbert Wibowo <filbertstudy@gmail.com>
2025-08-26 14:21:52 +0700

    Create filbert_project1.java

---
+++ Semester 3/PBO/filbert_project1.java
@@ -0,0 +1,8 @@
+public class filbert_project1 {
+    public static void main(String[] args) {
+        System.out.println("Nama   : Filbert Wibowo");
+        System.out.println("NPM    : 2440506049");
+        System.out.println("Alamat : Jl. Mawar No. 123, Semarang");
+        System.out.println("No. HP : 08123456789");
+    }
+}
